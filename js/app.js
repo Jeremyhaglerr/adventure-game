e64313e8d4110527790f5090a47314e0788853f9
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-02-09 17:22:23 -0600

    sanity check importing from reference-arrays

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*-------------------------------- Constants --------------------------------*/
+import { getRandomEncounter, startingClasses} from "../data/reference-arrays.js"
+console.log(getRandomEncounter())//logging result of getRandomEncounter 
+console.log(startingClasses);
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
