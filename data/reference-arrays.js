82654e7ea9765426e08407fab9741b07424884d6
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-02-09 19:29:26 -0600

    organize,  add objects, complete treasure logic

--- data/reference-arrays.js
+++ data/reference-arrays.js
@@ -1,3 +1,5 @@
+/*---------------------------- Game Arrays ----------------------------*/
+
 const randomEncounters = [
   {
     name:'Skeleton',
@@ -49,65 +51,76 @@ const randomEncounters = [
   }
 ]
 
-const startingClasses = [
-{
+/*---------------------------- Game Objects ----------------------------*/
+
+const Fighter = {
   name: 'Fighter',
   health:20,
   lowAttackRange:6,
   highAttackRange:10,
   defense:4
-},
-{
+}
+
+const Ranger = {
   name: 'Ranger',
   health:10,
   lowAttackRange:8,
   highAttackRange:14,
-  defense:2
-},
-]
+  defense:4
+}
+
+const optionalBoss = {
+  name: 'Lich',
+  health:20,
+  lowAttackRange:10,
+  highAttackRange:14,
+  defense:5
+}
+
+const mainBoss = {
+name: 'Uthvard the Giant King',
+health:30,
+lowAttackRange:8,
+highAttackRange: 14,
+defense:5
+}
+
+/*---------------------------- Value Generators ----------------------------*/
 
 function getWeaponChance() {
   let weaponChance= [true, false, false]
-  return weaponChance[Math.floor(Math.random() * weaponChance.length)]
+  let lowDamage = [1,2,3,4,5,6,7,8,9]
+  let highDamage= [10,11,12,13,14,15,16]
+  if (weaponChance[Math.floor(Math.random() * weaponChance.length)]=== true) {
+    return[(lowDamage[Math.floor(Math.random() * lowDamage.length)]),(highDamage[Math.floor(Math.random() * highDamage.length)])]
+  } else return false
+
 }
-// console.log(getWeaponChance());
 
 function getArmorChance() {
   let armorChance= [true, false, false]
-  return armorChance[Math.floor(Math.random() * armorChance.length)]
+  let armorDefense= [3,4,5,6,7]
+  if (armorChance[Math.floor(Math.random() * armorChance.length)] === true){
+    return armorDefense[Math.floor(Math.random() * armorDefense.length)]
+  } else return false
 }
-// console.log(getArmorChance());
 
 function getPotionNumber() {
   let potionNumber= [1,2,3,4]
   return potionNumber[Math.floor(Math.random() * potionNumber.length)]
 }
-// console.log(getPotionNumber());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function getRandomEncounter() {
   return randomEncounters[Math.floor(Math.random() *randomEncounters.length)]
 }
+
+/*---------------------------- Exports ----------------------------*/
+
 export{
   getRandomEncounter,
-  startingClasses
+  Fighter,
+  Ranger,
+  optionalBoss,
+  mainBoss
 }
