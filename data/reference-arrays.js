43945e6b6649776b0d3f61afc7344187cf3dde84
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-02-09 18:06:47 -0600

    add treasure to array with base functionality

--- data/reference-arrays.js
+++ data/reference-arrays.js
@@ -40,6 +40,12 @@ const randomEncounters = [
     lowAttackRange:2,
     highAttackRange:5,
     defense:5,
+  },
+  {
+    name:'Treasure',
+    hasWeapon: getWeaponChance(),
+    hasArmor: getArmorChance(),
+    potion: getPotionNumber(),
   }
 ]
 
@@ -57,8 +63,27 @@ const startingClasses = [
   lowAttackRange:8,
   highAttackRange:14,
   defense:2
-}
+},
 ]
+
+function getWeaponChance() {
+  let weaponChance= [true, false, false]
+  return weaponChance[Math.floor(Math.random() * weaponChance.length)]
+}
+// console.log(getWeaponChance());
+
+function getArmorChance() {
+  let armorChance= [true, false, false]
+  return armorChance[Math.floor(Math.random() * armorChance.length)]
+}
+// console.log(getArmorChance());
+
+function getPotionNumber() {
+  let potionNumber= [1,2,3,4]
+  return potionNumber[Math.floor(Math.random() * potionNumber.length)]
+}
+// console.log(getPotionNumber());
+
 
 
 
@@ -80,7 +105,7 @@ const startingClasses = [
 
 
 function getRandomEncounter() {
-  return randomEncounters[Math.floor(Math.random(randomEncounters.length))]
+  return randomEncounters[Math.floor(Math.random() *randomEncounters.length)]
 }
 export{
   getRandomEncounter,
