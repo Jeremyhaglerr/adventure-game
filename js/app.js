e2704e87b72d98c8e8c42ef871e63ae7537d2d5b
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-02-09 19:27:00 -0600

    refactor imports and initialize state variables

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
-import { getRandomEncounter, startingClasses} from "../data/reference-arrays.js"
+
+import { getRandomEncounter, Fighter, Ranger, optionalBoss, mainBoss } from "../data/reference-arrays.js"
 console.log(getRandomEncounter())//logging result of getRandomEncounter 
-console.log(startingClasses);
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+let currentPlayerHealth,
+  currentPlayerLowAttack,
+  CurrentPlayerHighAttack,
+  currentPLayerDefense,
+  currentPlayerPotions,
+  currentRoomsSurvived
 
+let currentEnemyName,
+  currentEnemyHealth,
+  currentEnemyLowAttack,
+  currentEnemyHighAttack,
+  currentEnemyDefense
 
 /*------------------------ Cached Element References ------------------------*/
 
